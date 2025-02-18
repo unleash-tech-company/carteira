5e9b9ae1df6e105a9936086fa28b204cb350115d
Luis Felipe Sena <luisfelipesena@gmail.com>
2025-02-17 21:12:06 -0300

    fix route

--- src/server/zero/client.ts
+++ src/server/zero/client.ts
@@ -12,7 +12,7 @@ export function createClient(userID: string): Client {
   }
 
   const baseUrl = process.env.NEXT_PUBLIC_APP_URL || window.location.origin;
-  const serverUrl = `${baseUrl}/api/zero`;
+  const serverUrl = `${baseUrl}/api`;
 
   if (!clientInstance) {
     clientInstance = new Zero({
