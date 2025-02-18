7a74b94f0f66249dd7f3fbdf28d6e7c2a18f0f6e
Luis Felipe Sena <luisfelipesena@gmail.com>
2025-02-18 00:16:58 -0300

    a

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,16 +1,22 @@
 import pino from 'pino'
 
-const logger = pino({
-  transport: {
-    target: 'pino-pretty',
-    options: {
-      colorize: true,
-      ignore: 'pid,hostname',
-      translateTime: 'UTC:yyyy-mm-dd HH:MM:ss.l o',
-      showLevel: true,
+const logger = process.env.NODE_ENV === 'production'
+  ? {
+    debug: () => { },
+    info: () => { },
+    warn: () => { },
+    error: () => { }
+  } : pino({
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        ignore: 'pid,hostname',
+        translateTime: 'UTC:yyyy-mm-dd HH:MM:ss.l o',
+        showLevel: true,
+      },
     },
-  },
-  level: process.env.NODE_ENV === 'development' ? 'debug' : 'info',
-})
+    level: 'debug'
+  })
 
-export default logger 
+export default logger
